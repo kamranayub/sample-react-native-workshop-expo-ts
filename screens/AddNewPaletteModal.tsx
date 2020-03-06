207cc0cf75398663f43851fcdb898305cf07115e
Kamran Ayub <kamran.ayub@gmail.com>
2020-03-06 15:58:45 -0600

    add input

--- screens/AddNewPaletteModal.tsx
+++ screens/AddNewPaletteModal.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { spacing } from '../tokens';
 
-const AddNewPaletteModal = () => (
-  <View>
-    <Text>Hello World!</Text>
-  </View>
-);
+const AddNewPaletteModal = () => {
+  const [name, setName] = React.useState('');
+
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+      <TextInput style={styles.input} value={name} onChangeText={setName} />
+    </View>
+  );
+};
 
 export default AddNewPaletteModal;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: spacing.x4,
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  input: {
+    borderColor: 'gray',
+    borderWidth: 1,
+    padding: spacing.x2,
+  },
+});
